a2bf149045323c732f6e86d2546b798a01c50dc8
xmarimarquesh <marimarquesh39@gmail.com>
2024-09-13 14:54:55 -0300

    EXERCICIO 6

---
+++ exer6.py
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+# //exercicio 8 aula pr�tica
+# int main(){
+# 	int age;
+	
+# 	printf("Qual a idade do aluno:");
+# 	scanf("%d", &age);
+# 	printf("A idade Informada foi: %d \n", age);
+	
+# 	system("pause");
+# 	return 0;
+# }
+
+idade = input("Qual a idade do aluno: ")
+print("A idade informada foi: " + idade)
